17efd722277eb76fd5124ecfcf4bbcb996b99467
Vladyslav <ghostofprocrastination@gmail.com>
2024-10-31 00:20:53 +0200

    Binary search.

---
+++ binary_search.py
@@ -0,0 +1,23 @@
+import math
+
+
+def binary_serch(items, target):
+    low = 0
+    high = len(items) - 1
+
+    while low <= high:
+        mid = math.ceil((high + low) / 2)
+        candidate = items[mid]
+
+        print(candidate, mid, low, high)
+
+        if candidate == target:
+            return mid
+
+        if candidate > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+
+print(binary_serch([1, 2, 3, 4, 5, 6, 7, 8], 6))
